2c6955cddad0a4163e03b800ec8b176ecae62866
ivynguyen1998 <58804369+ivynguyen1998@users.noreply.github.com>
2020-11-18 17:53:43 -0800

    Update hostInfo.py

--- wgprivnet/hostInfo.py
+++ wgprivnet/hostInfo.py
@@ -24,3 +24,28 @@ class hostInfo(object):
             'IP': self.ip,
             'port': self.port,
         })
+    
+  From Tkinter import *
+	top = Login()
+	Username = StringVar()
+	Password = StringVar()
+	L1 = Label(top, text = “Username:”)
+	L1.pack(side = LEFT)
+	E1 = Entry(top, textvariable = Username, bd = 5)
+	E1.pack(side = RIGHT)
+    L2 = Label(top, text = “Password:”)
+	L2.pack(side = LEFT)
+	E2 = Entry(top, textvariable = Password, bd = 5)
+	E2.pack(side = RIGHT)
+	Bool = Verify(Username, Password)
+	if(Bool)
+		Login()
+	top.mainloop()
+
+	verify(Username, Password)
+	Names = [‘default’]
+	Passwords = [‘default’]
+if (Username in Names[0] and Password in Passwords[0]:
+    return true	
+else 
+	return false
