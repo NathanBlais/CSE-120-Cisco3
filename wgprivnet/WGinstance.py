5014aef3f52f1848f6b655ccea0f4db190ec1489
Orion Johnson <ojohnson2@ucmerced.edu>
2020-12-01 22:14:22 -0800

    Update WGinstance.py

--- wgprivnet/WGinstance.py
+++ wgprivnet/WGinstance.py
@@ -18,12 +18,12 @@ class WireGuard(object):
     def change_dns(path, IP_Addr):
         process = subprocess.run(['./../scripts/change_DNS.sh', path, IP_Addr], stdout=subprocess.PIPE)
 
-    def list_peers():
+    def list_peers(self):
         process = subprocess.run(['./../scripts/list_peers.sh'], stdout=subprocess.PIPE)
 
     def make_client_config(username):
         process=subprocess.run(['./client_conf_make.sh', username], stdout=subprocess.PIPE)
 
-    def login():
+    def login(self):
         userLogin.login()
         
