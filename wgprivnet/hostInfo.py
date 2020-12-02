d430699ed191ef23a0b3b501697f3ce26b87c807
Orion Johnson <ojohnson2@ucmerced.edu>
2020-12-01 20:01:48 -0800

    Update hostInfo.py

--- wgprivnet/hostInfo.py
+++ wgprivnet/hostInfo.py
@@ -2,26 +2,28 @@
 From Tkinter import *
 
 class hostInfo(object):
-	top = Login()
-	Username = StringVar()
-	Password = StringVar()
-	L1 = Label(top, text = “Username:”)
-	L1.pack(side = LEFT)
-	E1 = Entry(top, textvariable = Username, bd = 5)
-	E1.pack(side = RIGHT)
-    	L2 = Label(top, text = “Password:”)
-	L2.pack(side = LEFT)
-	E2 = Entry(top, textvariable = Password, bd = 5)
-	E2.pack(side = RIGHT)
-	Bool = Verify(Username, Password)
-	if(Bool)
-		Login()
-	top.mainloop()
+	
+	def login_user():
+		top = Login()
+		Username = StringVar()
+		Password = StringVar()
+		L1 = Label(top, text = “Username:”)
+		L1.pack(side = LEFT)
+		E1 = Entry(top, textvariable = Username, bd = 5)
+		E1.pack(side = RIGHT)
+		L2 = Label(top, text = “Password:”)
+		L2.pack(side = LEFT)
+		E2 = Entry(top, textvariable = Password, bd = 5)
+		E2.pack(side = RIGHT)
+		Bool = Verify(Username, Password)
+		if(Bool)
+			Login()
+		top.mainloop()
 
-	verify(Username, Password)
-	Names = [‘default’]
-	Passwords = [‘default’]
-	if (Username in Names[0] and Password in Passwords[0]:
-	    return true	
-	else 
-		return false
+		verify(Username, Password)
+		Names = [‘default’]
+		Passwords = [‘default’]
+		if (Username in Names[0] and Password in Passwords[0]:
+		    return true	
+		else 
+			return false
