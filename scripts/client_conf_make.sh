ed8ac5554509b17bb0029c04bbadad892fa5c963
Nathan Blais <nathanblais@Nathans-MBP-Lan.lan>
2020-12-01 21:03:32 -0800

    d

--- scripts/client_conf_make.sh
+++ scripts/client_conf_make.sh
@@ -6,7 +6,7 @@
 #How to use this script: 
 : '
     When you call the script it should be used like this:
-       ./client_conf_make.sh /path/to/server/config/wg0.conf "name of client"
+       ./client_conf_make.sh "name of client"
 	   
 '
 
