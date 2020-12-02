02fc94aecdffe23b9b369c86b19fc8ef6aa07eac
Orion Johnson <ojohnson2@ucmerced.edu>
2020-12-01 22:16:03 -0800

    Update WGinstance.py

--- wgprivnet/WGinstance.py
+++ wgprivnet/WGinstance.py
@@ -4,26 +4,26 @@ import sys
 
 from pyroute2 import netlink
 
-from setupQrKey import setupQrKey
-from setupPubKey import setupPubKey
+#from setupQrKey import setupQrKey
+#from setupPubKey import setupPubKey
 from hostInfo import hostInfo
 
 userLogin=hostInfo()
 
 class WireGuard(object):
-           
-    def remove_client(username):
-        process = subprocess.run(['./../scripts/remove_client.sh', username], stdout=subprocess.PIPE)
-        
-    def change_dns(path, IP_Addr):
-        process = subprocess.run(['./../scripts/change_DNS.sh', path, IP_Addr], stdout=subprocess.PIPE)
 
-    def list_peers(self):
-        process = subprocess.run(['./../scripts/list_peers.sh'], stdout=subprocess.PIPE)
+           def remove_client(username):
+                      process = subprocess.run(['./../scripts/remove_client.sh', username], stdout=subprocess.PIPE)
 
-    def make_client_config(username):
-        process=subprocess.run(['./client_conf_make.sh', username], stdout=subprocess.PIPE)
+           def change_dns(path, IP_Addr):
+                      process = subprocess.run(['./../scripts/change_DNS.sh', path, IP_Addr], stdout=subprocess.PIPE)
 
-    def login(self):
-        userLogin.login()
-        
+           def list_peers(self):
+                      process = subprocess.run(['./../scripts/list_peers.sh'], stdout=subprocess.PIPE)
+
+           def make_client_config(username):
+                      process=subprocess.run(['./client_conf_make.sh', username], stdout=subprocess.PIPE)
+
+           def login(self):
+                      userLogin.login()
+
