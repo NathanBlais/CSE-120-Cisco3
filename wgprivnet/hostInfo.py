f0ed4495bb461320c1fd921f5dfb2eae538465d2
ivynguyen1998 <58804369+ivynguyen1998@users.noreply.github.com>
2020-12-01 21:39:00 -0800

    Update hostInfo.py

--- wgprivnet/hostInfo.py
+++ wgprivnet/hostInfo.py
@@ -3,27 +3,47 @@ from tkinter import *
 
 class hostInfo(object):
 	
-	def login_user():
-		top = Login()
-		Username = StringVar()
-		Password = StringVar()
-		L1 = Label(top, text = “Username:”)
-		L1.pack(side = LEFT)
-		E1 = Entry(top, textvariable = Username, bd = 5)
-		E1.pack(side = RIGHT)
-		L2 = Label(top, text = “Password:”)
-		L2.pack(side = LEFT)
-		E2 = Entry(top, textvariable = Password, bd = 5)
-		E2.pack(side = RIGHT)
-		Bool = Verify(Username, Password)
-		if(Bool)
-			Login()
-		top.mainloop()
+def login_user():
 
-		verify(Username, Password)
-		Names = [‘default’]
-		Passwords = [‘default’]
-		if (Username in Names[0] and Password in Passwords[0]:
-		    return true	
-		else 
-			return false
+def Verify(Username, Password):
+    f = open("username.txt", "r")
+    h = open("password.txt", "r")
+    if Username in f and Password in h:
+        return True
+
+def callback(Username, Password):
+    with open("username.txt", "a+") as fh:
+        isduplicate = open("username.txt").read().find(Username)
+        if isduplicate != -1:
+            print("Duplicate found")
+            return
+        fh.write(Username+("\n"))
+    with open("password.txt", "a+") as fh:
+        fh.write(Password+("\n"))
+
+def login():
+    m = Tk()
+    m.geometry("400x200")
+    m.title("Create Administer User")
+
+    Username = StringVar()
+    Password = StringVar()
+    L1 = Label(m, text= "Username:")
+    L1.place(x = 15, y = 10)
+    E1 = Entry(m, textvariable=Username, bd=5, width = "20")
+    E1.pack(side=TOP)
+    L2 = Label(m, text= "Password:")
+    L2.place(x = 15, y = 50)
+    E2 = Entry(m, textvariable=Password, bd=5, width = "20")
+    E2.pack(side=TOP)
+    B = Button(m, text="Create Administer", width="20", height="1", command=lambda:callback(E1.get(), E2.get()))
+    B.pack()
+
+    Bool = Verify(Username, Password)
+    if (Bool):
+        login()
+    m.mainloop()
+
+login()
+
+
